7aceb149b137433da2324dbf86b7c4ec1e2ed55c
KateProtasevich <KrishniovaKate@mail.ru>
2020-03-06 16:34:11 +0300

    feat: add carbon task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,20 @@
-const MODERN_ACTIVITY= 15; 
+const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (typeof sampleActivity != "string" || arguments.length == 0) {
+    return false;
+  }
+  
+  if (sampleActivity.match(/(\d+\.\d+)\.\d+/)) {
+    let number = sampleActivity.match(/(\d+\.\d+)\.\d+/);
+    sampleActivity = number[1];
+  }
+
+  if (isNaN(sampleActivity) || sampleActivity<=0 || sampleActivity>15) {
+    return false;
+  }
+  let k = 0.693/HALF_LIFE_PERIOD;
+  return Math.ceil(Math.log(MODERN_ACTIVITY/sampleActivity)/k);
+
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,75 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+  let obj_arr = [];
+  let new_arr = [];
+  let obj = {};
+  let del_count = 0;
+  let position;
+    if (!(Array.isArray(arr))) {
+      throw Error;
+    }
+
+
+    arr.forEach( function(item, i) {
+      if (typeof item === "string") {
+
+      obj.str = item;
+      obj.position = i;
+      obj_arr.push(obj);
+      obj = {};
+      }
+    });
+
+    new_arr = arr;
+
+    obj_arr.forEach(function(init)  {
+      if (init.str === '--double-prev') {
+        if (init.position - del_count > 0) {
+          new_arr[init.position-del_count] = new_arr[init.position-del_count-1];
+        } else {
+          new_arr.splice(init.position-del_count,1);
+          del_count++;
+        }
+
+      } else
+
+
+      if (init.str === '--double-next') {
+        if (init.position - del_count < new_arr.length-1) {
+        new_arr[init.position-del_count] = new_arr[init.position+1-del_count];
+      } else {
+        new_arr.splice(init.position-del_count,1);
+        del_count++;
+      }
+
+
+      }  else
+
+
+      if (init.str === '--discard-prev') {
+        if (init.position - del_count > 0) {
+          new_arr.splice(init.position-del_count-1,1);
+          del_count++;
+        }
+        new_arr.splice(init.position-del_count,1);
+        del_count++;
+      } else
+
+
+      if (init.str === '--discard-next') {
+        if (init.position - del_count < new_arr.length-1) {
+          new_arr.splice(init.position-del_count+1,1);
+          del_count++;
+        } else {
+          new_arr.splice(init.position-del_count+1,1);
+          new_arr.splice(init.position-del_count,1);
+        }
+        new_arr.splice(init.position-del_count+1,1);
+        del_count++;
+      }
+
+
+
+    });
+
+    return new_arr;
 };
